ccd6a41e89ebc16a6732dcdc472a70ba3ddff6a1
Kiki <kiki@ebconnect.com>
2025-02-17 16:16:22 +0700

    feat: initialize routes api

--- src/app.tsx
+++ src/app.tsx
@@ -14,19 +14,95 @@
  * limitations under the License.
 */
 
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import { createRoot } from "react-dom/client";
-import { APIProvider, Map, MapCameraChangedEvent} from '@vis.gl/react-google-maps';
+import { APIProvider, Map, MapCameraChangedEvent, useMap, useMapsLibrary} from '@vis.gl/react-google-maps';
+
+const Directions = () => {
+  const map = useMap();
+  const routesLibrary = useMapsLibrary('routes');
+  const [directionsService, setDirectionsService] =
+    useState<google.maps.DirectionsService>();
+  const [directionsRenderer, setDirectionsRenderer] =
+    useState<google.maps.DirectionsRenderer>();
+  const [routes, setRoutes] = useState<google.maps.DirectionsRoute[]>([]);
+  const [routeIndex, setRouteIndex] = useState(0);
+  const selected = routes[routeIndex];
+  const leg = selected?.legs[0];
+
+  // Initialize directions service and renderer
+  useEffect(() => {
+    if (!routesLibrary || !map) return;
+    setDirectionsService(new routesLibrary.DirectionsService());
+    setDirectionsRenderer(
+      new routesLibrary.DirectionsRenderer({
+        draggable: false, // Only necessary for draggable markers
+        map
+      })
+    );
+  }, [routesLibrary, map]);
+
+  // Use directions service
+  useEffect(() => {
+    if (!directionsService || !directionsRenderer) return;
+
+    directionsService
+      .route({
+        origin: 'Ebco Workshop',
+        destination: 'Monas',
+        travelMode: google.maps.TravelMode.DRIVING,
+        provideRouteAlternatives: true
+      })
+      .then(response => {
+        directionsRenderer.setDirections(response);
+        setRoutes(response.routes);
+      });
+
+    return () => directionsRenderer.setMap(null);
+  }, [directionsService, directionsRenderer]);
+
+  // Update direction route
+  useEffect(() => {
+    if (!directionsRenderer) return;
+    directionsRenderer.setRouteIndex(routeIndex);
+  }, [routeIndex, directionsRenderer]);
+
+  if (!leg) return null;
+
+  return (
+    <div className="directions">
+      <h2>{selected.summary}</h2>
+      <p>
+        {leg.start_address.split(',')[0]} to {leg.end_address.split(',')[0]}
+      </p>
+      <p>Distance: {leg.distance?.text}</p>
+      <p>Duration: {leg.duration?.text}</p>
+
+      <h2>Other Routes</h2>
+      <ul>
+        {routes.map((route, index) => (
+          <li key={route.summary}>
+            <button onClick={() => setRouteIndex(index)}>
+              {route.summary}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
 
 const App = () => (
   <APIProvider apiKey={'AIzaSyAQOaOB1jnoSdf-hqIvWFxzn5HkNzg8_sI'} onLoad={ () => console.log('Maps API has loaded')} >
     <Map 
       defaultZoom={13}
       defaultCenter={ { lat:-6.262273750432054, lng: 106.83121588674571 }}
-      mapId='4af6d8528552277'
+      mapId={'4af6d8528552277'}
+      disableDefaultUI={true}
       onCameraChanged={ (ev: MapCameraChangedEvent) => 
         console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
       }>
+      <Directions />
     </Map>
   </APIProvider>
 );
